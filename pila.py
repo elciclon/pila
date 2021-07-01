d26d9e43bbef52df4ca1c2842545b58188008fe1
elciclon <adrianfernandezfazio@gmail.com>
2021-07-01 20:31:58 -0300

    Implementing the methods of the class

--- pila.py
+++ pila.py
@@ -1,8 +1,23 @@
 class Pila(object):
     """Implementación de una Pila."""
-    def __init__(self, contenido):
-        self.contenido = contenido
-        self.acumulador = []
+    def __init__(self, *args):
+        self.contenido = []
+        for arg in args:
+            self.contenido.append(arg)
+        
+    def __str__(self) -> str:
+        rep = 'El contenido de la pila es ' + str(self.contenido)
+        return rep
+
+    def top(self):
+        return self.contenido[-1]    
+    
+    def push(self, *args):
+        for arg in args:
+            self.contenido.append(arg)
+        
+    def pop(self):
+        pass
 
 
         
